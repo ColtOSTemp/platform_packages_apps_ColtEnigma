a46a1d3b44e6e74530a2913ee5b685dc7a35ab5f
ZeNiXxX <zenixxx.havoc@gmail.com>
2020-12-29 13:54:44 +0700

    enigma: Introduce awesome brightness slider and UI style [3/3]
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/colt/enigma/fragments/ColtTheme.java
+++ src/com/colt/enigma/fragments/ColtTheme.java
@@ -65,6 +65,7 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.colt.enigma.display.QsTileStylePreferenceController;
 import com.colt.enigma.display.SwitchStylePreferenceController;
+import static com.colt.enigma.utils.UtilsColt.handleOverlays;
 import com.android.internal.util.colt.ColtUtils;
 import com.android.internal.util.colt.ThemesUtils;
 
@@ -89,6 +90,8 @@ public class ColtTheme extends DashboardFragment implements
     private static final String PREF_KEY_CUTOUT = "cutout_settings";
 
     private static final String PREF_HD_SIZE = "header_size";
+    private static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+    private static final String UI_STYLE = "ui_style";
 
     static final int DEFAULT = 0xff1a73e8;
 
@@ -97,7 +100,8 @@ public class ColtTheme extends DashboardFragment implements
     private ColorPickerPreference mThemeColor;
     private ColorPickerPreference mGradientColor;
     private ListPreference mhdSize;
-
+    private ListPreference mBrightnessSliderStyle;
+    private ListPreference mUIStyle;
     private IntentFilter mIntentFilter;
     private static FontPickerPreferenceController mFontPickerPreference;
 
@@ -124,6 +128,66 @@ public class ColtTheme extends DashboardFragment implements
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
+	mUIStyle = (ListPreference) findPreference(UI_STYLE);
+        int UIStyle = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.UI_STYLE, 0);
+        int UIStyleValue = getOverlayPosition(ThemesUtils.UI_THEMES);
+        if (UIStyleValue != 0) {
+            mUIStyle.setValue(String.valueOf(UIStyle));
+        }
+        mUIStyle.setSummary(mUIStyle.getEntry());
+        mUIStyle.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if (preference == mUIStyle) {
+                    String value = (String) newValue;
+                    Settings.System.putInt(getActivity().getContentResolver(), Settings.System.UI_STYLE, Integer.valueOf(value));
+                    int valueIndex = mUIStyle.findIndexOfValue(value);
+                    mUIStyle.setSummary(mUIStyle.getEntries()[valueIndex]);
+                    String overlayName = getOverlayName(ThemesUtils.UI_THEMES);
+                    if (overlayName != null) {
+                    handleOverlays(overlayName, false, mOverlayService);
+                    }
+                    if (valueIndex > 0) {
+                        handleOverlays(ThemesUtils.UI_THEMES[valueIndex],
+                                true, mOverlayService);
+                    }
+                    return true;
+                }
+                return false;
+            }
+       });
+
+        mBrightnessSliderStyle = (ListPreference) findPreference(BRIGHTNESS_SLIDER_STYLE);
+        int BrightnessSliderStyle = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.BRIGHTNESS_SLIDER_STYLE, 0);
+        int BrightnessSliderStyleValue = getOverlayPosition(ThemesUtils.BRIGHTNESS_SLIDER_THEMES);
+        if (BrightnessSliderStyleValue != 0) {
+            mBrightnessSliderStyle.setValue(String.valueOf(BrightnessSliderStyle));
+        }
+        mBrightnessSliderStyle.setSummary(mBrightnessSliderStyle.getEntry());
+        mBrightnessSliderStyle.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                if (preference == mBrightnessSliderStyle) {
+                    String value = (String) newValue;
+                    Settings.System.putInt(getActivity().getContentResolver(), Settings.System.BRIGHTNESS_SLIDER_STYLE, Integer.valueOf(value));
+                    int valueIndex = mBrightnessSliderStyle.findIndexOfValue(value);
+                    mBrightnessSliderStyle.setSummary(mBrightnessSliderStyle.getEntries()[valueIndex]);
+                    String overlayName = getOverlayName(ThemesUtils.BRIGHTNESS_SLIDER_THEMES);
+                    if (overlayName != null) {
+                    handleOverlays(overlayName, false, mOverlayService);
+                    }
+                    if (valueIndex > 0) {
+                        handleOverlays(ThemesUtils.BRIGHTNESS_SLIDER_THEMES[valueIndex],
+                                true, mOverlayService);
+                    }
+                    return true;
+                }
+                return false;
+            }
+       });
 
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
@@ -308,6 +372,28 @@ public class ColtTheme extends DashboardFragment implements
         else{
             mhdSize.setValue("1");
         }
+    }
+
+    private String getOverlayName(String[] overlays) {
+        String overlayName = null;
+        for (int i = 0; i < overlays.length; i++) {
+            String overlay = overlays[i];
+            if (ColtUtils.isThemeEnabled(overlay)) {
+                overlayName = overlay;
+            }
+        }
+        return overlayName;
+    }
+
+    private int getOverlayPosition(String[] overlays) {
+        int position = -1;
+        for (int i = 0; i < overlays.length; i++) {
+            String overlay = overlays[i];
+            if (ColtUtils.isThemeEnabled(overlay)) {
+                position = i;
+            }
+        }
+        return position;
     }
 
     @Override
